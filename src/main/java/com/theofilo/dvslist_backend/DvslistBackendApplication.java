ef49bacdfdb9ce483acc909dcffe2b5ff6bee9e1
Theofilo Henrique <theohenriquee01@gmail.com>
2025-08-20 14:38:38 -0300

    Project created

---
+++ src/main/java/com/theofilo/dvslist_backend/DvslistBackendApplication.java
@@ -0,0 +1,13 @@
+package com.theofilo.dvslist_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DvslistBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DvslistBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/theofilo/dvslist_backend/DvslistBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.theofilo.dvslist_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DvslistBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
